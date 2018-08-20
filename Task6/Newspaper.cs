c6e4595d983486957dab25256e521786443fc23e
Islam <islamrahimbaeff@outlook.com>
2018-08-20 16:10:34 +0600

    second

---
+++ Task6/Newspaper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Task6
+{
+    public class Newspaper : Publication
+    {
+        public long? Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string ISSN { get; set; }        
+
+        public override string ToString()
+        {
+            return base.ToString() + string.Format("ISSN: {0}\nId: {1}\nDate {2}",
+                !string.IsNullOrEmpty(ISSN) ? ISSN : "Unknown", 
+                Id.HasValue ? Id.Value.ToString() : "Unknown", 
+                Date.HasValue ? Date.Value.ToString("yyyy-MM-dd") : "Unknown");
+        }        
+
+        public override XElement SerializeToXEelement()
+        {
+            return new XElement(GetType().Name, GetAttributes().ToArray());
+        }
+    }
+}
